3eb7da926a0900e58d2bbcdfffa94054840e4d87
Jonah-arm <61660423+Jonah-arm@users.noreply.github.com>
2020-03-01 18:06:24 -0600

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,28 +1,35 @@
 import sys
+cache = {}
 
 def getAnswer(i, j):
     m = 0
     for n in range(i, j+1):
-        k = 1
-        while n != 1:
-            if n % 2 == 1:
-                n = 3 * n + 1
-                k += 1
-            else:
-                n = n / 2
-                k += 1
+        initial = n
+        if(n in cache):
+            k = cache[n]
+        else:   
+            k = 1
+            while n != 1:
+                if n % 2 == 1:
+                    n = 3 * n + 1
+                    k += 1
+                else:
+                    n = n / 2
+                    k += 1
         if k > m:
             m = k
+        cache[initial] = k
     return m
 
+def main():
+    for line in sys.stdin:
+        if not line:
+            break
+        i, j = [int(x) for x in line.split()]
+        if i > j:
+            i, j = (j, i)
+        maxlength = getAnswer(i, j)
+        print(i, j, maxlength)
 
-for l in sys.stdin:
-    if  len(l) == 0:
-        break
-    line = l.split()
-    i = int(line[0])
-    j = int(line[1])
-    k = getAnswer(i,j)
-    print(i, j, k)
-    l = input()
-
+if __name__ == '__main__':
+    main()
