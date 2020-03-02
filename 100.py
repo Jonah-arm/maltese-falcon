6c40b7e63b7a45ce2de5e861d33fdac0c0998bb0
Jonah-arm <61660423+Jonah-arm@users.noreply.github.com>
2020-03-01 18:56:24 -0600

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,24 +1,26 @@
 import sys
 cache = {}
 
+def length(n):
+    if n in cache:
+        return cache[n]
+    if n == 1:
+        return 1
+    orgin = n
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = 3*n + 1
+    count = length(n) + 1
+    cache[orgin] = count
+    return count
+
 def getAnswer(i, j):
     m = 0
-    for n in range(i, j+1):
-        initial = n
-        if(n in cache):
-            k = cache[n]
-        else:   
-            k = 1
-            while n != 1:
-                if n % 2 == 1:
-                    n = 3 * n + 1
-                    k += 1
-                else:
-                    n = n / 2
-                    k += 1
+    for n in range(i, j + 1):   
+        k = length(n)
         if k > m:
             m = k
-        cache[initial] = k
     return m
 
 def main():
@@ -27,9 +29,14 @@ def main():
             break
         i, j = [int(x) for x in line.split()]
         if i > j:
-            i, j = (j, i)
-        maxlength = getAnswer(i, j)
+            start_point = j
+            end_point = i
+        else:
+            start_point = i
+            end_point = j
+        maxlength = getAnswer(start_point, end_point)
         print(i, j, maxlength)
 
 if __name__ == '__main__':
     main()
+
